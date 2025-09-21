5554e59655178408e7978d89fab673e3080610cd
Madalina <madalinatusineanm@gmail.com>
2025-09-21 21:36:41 +0200

    Am rezolvat cum trebuie si exercitiul 4

--- sprint8-js/ex04.js
+++ sprint8-js/ex04.js
@@ -1,11 +1,14 @@
-let text = "Ana are mere";
-const vowels = ["a", "e", "i", "o", "u"];
+let text = "My program that has vowels";
+let vowels = ["a", "e", "i", "o", "u"];
+
 let sum = 0;
 
 for (i = 0; i < text.length; i++) {
     for (k = 0; k < vowels.length; k++) {
-        sum = sum + vowels[0];
+        if (text[i] === vowels[k]) {
+            sum += 1;
+        }
     }
 }
 
-console.log("The number of vowels in 'Ana are mere' is: " + sum);
+console.log("The amount of vowels in my program that has vowels is: ", sum);

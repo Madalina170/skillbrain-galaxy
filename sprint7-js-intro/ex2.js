3e09521a9706a2f053ce3bb1fce19de7dffe2ba5
Madalina170 <madalinatusineanm@gmail.com>
2025-10-06 19:51:57 +0200

    Update sprint7-js-intro/ex2.js
    Co-authored-by: Marcel <buianmarcel@gmail.com>

--- sprint7-js-intro/ex2.js
+++ sprint7-js-intro/ex2.js
@@ -2,4 +2,4 @@ function tellFortune (numarCopii, numePartener, locatie, profesie) {
     return "Vei fi un " + profesie + " in " + locatie + ", casatorit cu " + numePartener + " si vei avea " + numarCopii + " copii"
 }
 
-console.log(tellFortune(10, "Ana", "Filipine", "sofer de camion"))
+console.log(tellFortune(10, "Ana", "Filipine", "sofer de camion"));
